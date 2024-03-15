dedb2d58db180bb06b51466c877b2b4a9adf53ba
Alexandre A. Silva <alexdroide.developer@gmail.com>
2024-03-14 21:19:46 -0300

    feat: section-name reminds me of you - initial configuration

--- script.js
+++ script.js
@@ -8,4 +8,17 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log('Finished!');
         }
     });
-});
+});//desativei musica [com nome errado!]
+
+
+// section lembra-você
+    function videoslider(videoId) {
+      document.getElementById("video-player").innerHTML =
+        '<iframe width="560" height="315" src="https://www.youtube.com/embed/' +
+        videoId +
+        '?showinfo=0" controls: "0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+    }
+
+    window.onload = function () {
+      videoslider("kKfMjfogD7A");
+    };
