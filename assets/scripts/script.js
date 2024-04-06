7c1e9796f2f4e40d892b09d5fcfeec6df91b1804
Alexandre A. Silva <alexdroide.developer@gmail.com>
2024-04-06 17:14:12 -0300

    fix: mudança no script da musica de fundo

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,75 +1,62 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
-  // Array de caminhos das músicas de fundo
-  var musicasDeFundo = [
-    './assets/songs/fundo/It Must Have Been Love (acoustic cover) - Amber Leigh Irish (Official audio art).mp3',
-    './assets/songs/fundo/All My Life - Brian Nhira (K-Ci & JOJO)   The Sessions EP Pt. II.mp3'
-  ];
-
-  // Variável para controlar o índice da música atual
-  var musicaAtual = 0;
-
-  // Variável para armazenar o objeto Howl da música de fundo atual
-  var sound = null;
-
-  // Função para criar um novo objeto Howl com a música atual
-  function playNextSong() {
-    // Se houver uma instância anterior, pare-a
-    if (sound) {
-      sound.stop();
-    }
-
-    sound = new Howl({
-      src: [musicasDeFundo[musicaAtual]],
+  var sound = new Howl({
+      src: ['./assets/songs/fundo/All My Life - Brian Nhira (K-Ci & JOJO)   The Sessions EP Pt. II.mp3'],
       autoplay: true,
       loop: true,
       volume: 0.5,
       onend: function() {
-        console.log('Finished!');
+          console.log('Finished!');
       }
-    });
+  });
 
-    // Atualiza a variável musicaAtual para a próxima música no array
-    musicaAtual = (musicaAtual + 1) % musicasDeFundo.length;
-
-    // Retorna o objeto Howl criado
-    return sound;
-  }
+  var isSoundPaused = false; // Variável para controlar se o som está pausado
 
   // Função para pausar ou retomar o som
   function toggleSound() {
-    if (sound && sound.playing()) {
-      sound.pause();
-    } else {
-      playNextSong();
-    }
-    updateButton(); // Atualiza o botão
+      if (isSoundPaused) {
+          sound.play(); // Retoma o som
+          isSoundPaused = false; // Atualiza o estado do som
+      } else {
+          sound.pause(); // Pausa o som
+          isSoundPaused = true; // Atualiza o estado do som
+      }
+      updateButton(); // Atualiza o botão
   }
 
   // Função para atualizar o botão de acordo com o estado do som
   function updateButton() {
-    var button = document.getElementById('toggleSoundButton');
-    button.classList.toggle('clicked', sound && sound.playing()); // Adiciona ou remove a classe 'clicked' com base no estado do som
+      var button = document.getElementById('toggleSoundButton');
+      button.classList.toggle('clicked', isSoundPaused); // Adiciona ou remove a classe 'clicked' com base no estado do som
   }
 
   // Adiciona um evento de clique ao botão flutuante
   document.getElementById('toggleSoundButton').addEventListener('click', toggleSound);
 
+  // Atualiza o botão quando a música termina
+  sound.on('end', function() {
+      updateButton();
+  });
+
   // Pausa o som quando outra mídia for acionada
   var otherMediaElements = document.querySelectorAll('video, audio');
   otherMediaElements.forEach(function(mediaElement) {
-    mediaElement.addEventListener('play', function() {
-      if (sound && sound.playing()) {
-        sound.pause();
-        updateButton(); // Atualiza o botão
-      }
-    });
+      mediaElement.addEventListener('play', function() {
+          if (!isSoundPaused) {
+              sound.pause();
+          }
+      });
   });
-
+  
   // Inicializa o botão
   updateButton();
 });
 
 
+
+
+/* ---- */
 
 
 // section lembra-você
@@ -88,11 +75,11 @@ document.addEventListener("DOMContentLoaded", function() {
   const array = [
     "Minha linda!",
     "Minha Querida!",
-    "Meu Dilúvio de Amor!",
-    "Maravilhosa minha!",
+    "Dilúvio de Amor!",
+    "Maravilhosa!",
     "Amor da minha vida!",
+    "Educada!",
     "Tempesdade do bem!",
-    "Pessoa Educada!",
     "Respeitosa!",
     "Cheia de Luz!",
     "Abençoada!",
